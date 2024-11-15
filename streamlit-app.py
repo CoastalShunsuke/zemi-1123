ad13e8ce15b5df7cc35f7c1ec53f3be9f12962e1
CoastalShunsuke <syun0811nameko@gmail.com>
2024-11-16 03:35:14 +0900

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -4,6 +4,6 @@ import streamlit as st
 st.title("2024/11/23佐藤ゼミ資料")
 
 # セクション1: チャットアプリの起動と使用方法
-st.header("チャットアプリの起動と使用、\n性能の確認")
+st.header("チャットアプリの起動と使用、    性能の確認")
 st.write("以下の動画で、チャットアプリの起動と使用方法について説明しています。")
 st.video("https://zemi-movie.s3.us-west-2.amazonaws.com/20241123_video2.mp4")
