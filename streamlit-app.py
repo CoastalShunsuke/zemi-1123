311824f368b11e1df7feef4c18d00174796ac79b
CoastalShunsuke <syun0811nameko@gmail.com>
2024-11-16 03:34:24 +0900

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -1,4 +1,9 @@
 import streamlit as st
 
-# S3からの動画URLを指定して表示
+# タイトル
+st.title("2024/11/23佐藤ゼミ資料")
+
+# セクション1: チャットアプリの起動と使用方法
+st.header("チャットアプリの起動と使用、性能の確認")
+st.write("以下の動画で、チャットアプリの起動と使用方法について説明しています。")
 st.video("https://zemi-movie.s3.us-west-2.amazonaws.com/20241123_video2.mp4")
